5fa4b20be32f0464475555effafa1ce593bd1f07
José Manuel Oropeza Gallardo <jmoropeza.23@est.ucab.edu.ve>
2025-10-22 19:24:33 -0400

    Create futbol_data_staging and futbol_data tables
    Creates staging and production tables for futbol data.

---
+++ migrations/001_create_futbol_data.sql
@@ -0,0 +1,17 @@
+-- Archivo: 001_create_heart_data.sql
+
+-- Creación de la tabla de staging: heart_data_staging
+-- Esta tabla se utiliza para cargar los datos tal como vienen del CSV, sin restricciones de integridad
+-- (se permiten NULLs y duplicados). Posteriormente se aplicarán pruebas de calidad y se migrarán solo los
+-- registros válidos a la tabla de producción.
+CREATE TABLE IF NOT EXISTS futbol_data_staging (
+    -- #TODO: Agrega las columnas según el Data Dictionary del CSV
+);
+
+-- Creación de la tabla de producción: heart_data
+CREATE TABLE IF NOT EXISTS futbolt_data (
+    -- #TODO: Define la columna `id` como PRIMARY KEY.
+    -- #TODO: Agrega el resto de las columnas, estableciendo NOT NULL en cada una según el Data Dictionary del CSV.
+    -- #TODO: Agrega una restricción UNIQUE para evitar duplicados.
+);
+
